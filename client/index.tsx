a8c4a244dac1b61c32ca2d7c7753842937230a46
Noah Ibrahim <noahibr2@gmail.com>
2025-12-22 15:18:08 +0100

    pwa fix

--- client/index.tsx
+++ client/index.tsx
@@ -21,7 +21,7 @@ root.render(
 // PWA Service Worker Registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('/sw.js')
       .then((registration) => {
         console.log('SW registered: ', registration);
         
