aaad8907d3f2465b0841d3472331f6e421370d1d
Noah Ibrahim <noahibr2@gmail.com>
2026-06-02 17:08:20 +0100

    deleted unsueful

--- landinggin-main/src/services/api.ts
+++
@@ -1,7 +0,0 @@
-export const getBusinessCount = async (): Promise<{ count: number }> => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ count: 2134 });
-    }, 1000);
-  });
-};
